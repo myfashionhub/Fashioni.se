7c0d5c63fbd882f7a1fbacc8a66508a8ee5ac915
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-05-18 19:07:00 -0400

    Remove illegal char

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
 config.serve_static_assets = true
-config.action_dispatch.x_sendfile_header = ‘X-Accel-Redirect’
+config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 config.assets.compile = true
 
   #config.serve_static_assets = false
