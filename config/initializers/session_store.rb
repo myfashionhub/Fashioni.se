b5785347d6880892d5ff24dadeeb2389e111937e
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-05-20 21:08:20 -0400

    Take out time out in session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, 
-                                        key: '_fashionize_sessions',
-                                        :expire_after => 5.minutes
+                                        key: '_fashionize_sessions'
