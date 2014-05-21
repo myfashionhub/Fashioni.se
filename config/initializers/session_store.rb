9a7d0ddcd7e1fd17d674fcd7eba595f8547b238f
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-05-21 15:40:01 -0400

    Clear sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, 
-                                        key: '_fashionize_sessions'
+                                        key: '_change_me_sessions'
