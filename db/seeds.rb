1bdc2c916fef25a280e1bc2b69de17e2a0fc0454
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-07-22 18:10:18 -0400

    Edit path for edit profile on profile index

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,3 @@
   Style.create(name: style)
 end
 
-#Fashionista.create(username: 'myfashionhub', email: 'nessa_m00re@yahoo.com', tagline: 'Fashion for life', pic_url: 'http://www.nessanguyen.com/images/nessa.jpg', size: 'xs', budget: 'Under $50', style_id: 1)
-
-
-
